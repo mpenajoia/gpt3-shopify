3a8e4cf7482676c0f12c1741f126fce482bedeb7
mpenajoia <penajoia.marco@gmail.com>
2022-05-22 13:26:19 -0700

    prompt & responses

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,13 @@ function App() {
     presence_penalty: 0.0,
   }
 
-  // const [input, setInput] =useState('')
   const handleInput = (e) => {
     setPrompt(e.target.value)
   }
    
-  const [response, setResponse] = useState('')
   const [resList, setResList] = useState([])
   const handleButton = (e) => {
     e.preventDefault()
-    console.log(defaultData.prompt)
     fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
       method: "POST",
       headers: {
@@ -32,34 +29,30 @@ function App() {
     })
     .then(res => res.json())
     .then(data => {
-      setResponse(data.choices[0].text)
-      setResList([...resList, data.choices[0].text])
+      setResList([[prompt, data.choices[0].text], ...resList]);
     });
   }
-  console.log(resList)
   const resMap = resList?.map((each, key) => {
     return (
-      <li key={key} className="w-3/4 bg-blue-400 p-6 rounded text-white list-none my-4" >
-        <p>{each}</p>
+      <li key={key} className="w-3/4 bg-blue-400 p-6 rounded text-white list-none mt-4" >
+        <p>{each[0]}</p>
+        <p>{each[1]}</p>
       </li>
     )
   })
 
   return (
     <div className="flex justify-center items-center w-full">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center w-3/4">
         <h1 className="text-3xl font-bold text-center">Marco Silva's AI</h1>
         <form onSubmit={handleButton} className="flex flex-col w-3/4 items-center">
-          <input type='text' onChange={handleInput} value={prompt} placeholder='What shall I do?' className="w-1/2 text-center" />
+          <textarea rows="10" onChange={handleInput} value={prompt} placeholder='What shall I do?' className="w-full text-center" />
           <button type='submit'>Submit</button>
-
         </form>
-        <div className="flex justify-center">
-          {response ? 
-            <div className="w-3/4 bg-blue-400 p-6 rounded text-white">{response}</div> 
-            : ''}
+        <div className="flex flex-col items-center justify-center">
+          <h2>Responses</h2>
+          {resMap}
         </div>
-        {resMap}
       </div>
     </div>
   );
