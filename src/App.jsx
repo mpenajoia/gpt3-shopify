b426cf3fb0643dc3da7363e618d16359294f9f18
mpenajoia <penajoia.marco@gmail.com>
2022-05-22 14:40:04 -0700

    toggle engines

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,31 @@ function App() {
   const handleInput = (e) => {
     setPrompt(e.target.value)
   }
+  const engineOptions = {
+    0: "text-ada-001",
+    1: "text-babbage-001",
+    2: "text-curie-001",
+    3: "text-davinci-002"
+  }
+  const engineIQ = ['Simple', 'Average', 'Moderate', 'Profound']
+  const [currentEngine, setCurrentEngine] = useState('Moderate')
+  const handleEngines = (e) => {
+    setSelectedEngine(engineOptions[engineIQ.indexOf(e.target.value)])
+    setCurrentEngine(e.target.value)
+  }
+  const engineIQMap = engineIQ.map((item, key) => {
+    return (
+      <button key={key} onClick={handleEngines} value={item}>{item}</button>
+    )
+  })
 
+  const [selectedEngine, setSelectedEngine] = useState(engineOptions[2])
   
-  const [resList, setResList] = useState([])
 
+  const [resList, setResList] = useState([])
   const handleButton = (e) => {
     e.preventDefault()
-    fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+    fetch(`https://api.openai.com/v1/engines/${selectedEngine}/completions`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -37,8 +55,9 @@ function App() {
   const resMap = resList?.map((each, key) => {
     return (
       <li key={key} className="w-3/4 bg-blue-400 p-6 rounded text-white list-none mt-4" >
+        <p>Prompt:</p>
         <p>{each[0]}</p>
-        <p className="whitespace-pre-line	">{each[1]}</p>
+        <p className="whitespace-pre-line	">Response: {each[1]}</p>
       </li>
     )
   })
@@ -47,9 +66,18 @@ function App() {
   return (
     <div className="flex justify-center items-center w-full">
       <div className="flex flex-col items-center w-3/4">
-        <h1 className="text-3xl font-bold text-center">Marco Silva's AI</h1>
+        <h1 className="text-3xl font-bold text-center">How about a poem?</h1>
+        <div>
+          <h3>AI Engine Options</h3>
+          <div className="flex gap-3">
+            {engineIQMap}
+          </div>
+          <div>
+            <p>Current Engine Option: {currentEngine}</p>
+          </div>
+        </div>
         <form onSubmit={handleButton} className="flex flex-col w-3/4 items-center">
-          <textarea rows="10" onChange={handleInput} value={prompt} placeholder='What shall I do?' className="w-full text-center" />
+          <textarea rows="7" onChange={handleInput} value={prompt} placeholder='What shall I do?' className="w-full text-center" />
           <button type='submit'>Submit</button>
         </form>
         <div className="flex flex-col items-center justify-center">
